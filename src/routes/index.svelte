8579ce6194c4b25248d52173054de42a209f7fad
peterreeves <peter.x.reeves@gmail.com>
2022-02-17 11:01:55 +1100

    2 files modified, 1 added, 10 deleted

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,5 @@
-<script context="module">
-	export const prerender = true;
-</script>
-
 <script>
-	import Counter from '$lib/Counter.svelte';
+	export let id;
 </script>
 
 <svelte:head>
@@ -11,22 +7,9 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
+	<h1>Homepage</h1>
+	<a href="/visit?id=1">Visit 1</a>
+	<a href="/visit?id=2">Visit 2</a>
 </section>
 
 <style>
@@ -40,20 +23,5 @@
 
 	h1 {
 		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
---
+++ src/routes/visit.svelte
@@ -0,0 +1,48 @@
+<script context="module">
+	export const load = ({ url }) => {
+		return {
+			props: {
+				id: url.searchParams.get("id"),
+			}
+		}
+	};
+</script>
+
+<script>
+  export let id;
+  import { page } from "$app/stores";
+
+  const helper = (newId) => {
+    const searchParams = $page.url.searchParams;
+    searchParams.set("id", newId);
+    return "/visit?" + searchParams.toString();
+  }
+</script>
+
+<section>
+  <h1>Visit {id}</h1>
+  <h2>Works</h2>
+  <a href="/visit?id=1">Visit 1 (/visit?id=1)</a>
+  <a href="/visit?id=2">Visit 2 (/visit?id=2)</a>
+  <br>
+  <h2>Doesn't Work</h2>
+  <a href="{helper(1)}">Visit 1 ({helper(1)})</a>
+  <a href="{helper(2)}">Visit 2 ({helper(2)})</a>
+  <br>
+  <br>
+  <a href="/">Back to Homepage</a>
+</section>
+
+<style>
+  section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+  h1 {
+		width: 100%;
+	}
+</style>
